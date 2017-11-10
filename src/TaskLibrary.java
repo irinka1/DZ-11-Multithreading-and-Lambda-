a599fe6da15af5e9bab2426b180339ee11da6f25
Ирина <irenmast@gmail.com>
2017-11-10 16:31:02 +0200

    Update TaskLibrary.java

--- src/TaskLibrary.java
+++ src/TaskLibrary.java
@@ -2,27 +2,27 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-/*Написать программу которая считывает с консоли число peopleCount и maxAmount.
-Затем создает peopleCount потоков. Каждый поток - человек, хочет войти в библиотеку.
-Библиотека имеет лимит кол-ва людей которые одновременно могут в ней находиться - maxAmount.
-Каждый поток должен писать в консоль что он(человек) делает.
-пришел ко входу в библиотеку
-ждет входа в библиотеку (происходит только если нет места на момент прихода к библиотеке)
-вошел в библиотеку
-читает книгу (поток должен делать это действие рандомное кол-во времени от 1 до 5 секунд)
-вышел из библиотеки
+/*Write a program that reads the number of peopleCount and maxAmount from the console.
+Then creates peopleCount streams. Every stream is a person, wants to enter the library.
+The library has a limit of count of people who can simultaneously be in it - maxAmount.
+Each thread should write to the console that it (the person) does.
+came to the entrance to the library
+waiting for the entrance to the library (occurs only if there is no place at the time of coming to the library)
+entered the library
+reads the book (the thread should do this action randomly the amount of time from 1 to 5 seconds)
+left the library
 */
 
 
-/*Надо добавить в программу дверь - вход/выход в библиотеку.
-В дверь может одновременно проходить 1 поток. Время прохождения через дверь потока = 0.5 секунды).
-Появляются дополнительные действия со стороны поток которые они должны писать в консоль:
-подошел к двери с улицы
-подошел к двери изнутри
-проходит через дверь внутрь
-проходит через дверь наружу
-прошел через дверь внутрь
-прошел через дверь наружу
+/*It is necessary to add a door to the program - entrance / exit to the library.
+The door can simultaneously pass 1 flow. Time of passage through the door of the flow = 0.5 seconds).
+There are additional actions on the part of the thread that they should write to the console:
+went to the door from the street
+approached the door from the inside
+passes through the door inside
+passes through the door to the outside
+went through the door inside
+walked through the door outside
 */
 
 public class TaskLibrary {
@@ -30,29 +30,29 @@ public class TaskLibrary {
     public static void main(String[] args) throws InterruptedException {
 
 
-        System.out.println("Введите число человек, которые зашли");
+        System.out.println("Enter the number of people who entered");
         Scanner sc = new Scanner(System.in);
         int peopleCount = sc.nextInt();
 
-        System.out.println("Введите максимальное число человек, которые зайти в библиотеку");
+        System.out.println("Enter the maximum number of people who can access the library");
         Scanner sc2 = new Scanner(System.in);
         int maxAmount = sc2.nextInt();
 
         Semaphore semaphore = new Semaphore(peopleCount);
 
         semaphore.acquire();
-        new Thread(() -> System.out.println("Человек подошел к двери библиотеки")).start();
+        new Thread(() -> System.out.println("The man went to the library door")).start();
         semaphore.release();
 
         if (semaphore.availablePermits() ==0){
 
-            System.out.println("Человек стоит у двери");
+            System.out.println("A man is standing at the door");
         }
         else {
             semaphore.acquire();
-            System.out.println("Человек заходит в библиотеку через дверь внутрь");
+            System.out.println("A person enters the library through the door inside");
             Thread.sleep(500);
-            System.out.println("Человек прошел через дверь внутрь");
+            System.out.println("The man went through the door inside");
             semaphore.release();
 
         }
@@ -63,16 +63,16 @@ public class TaskLibrary {
 
             new Thread(() -> {
 
-                System.out.println("Человек №" + x + " заходит в библиотеку");
+                System.out.println("The man №" + x + " visits the library");
             }).start();
             semaphore.release();
         }
 
 
         if(peopleCount < maxAmount) {
-            new Thread(() -> System.out.println("Человек заходит в библиотеку, так как есть свободное место")).start();
+            new Thread(() -> System.out.println("A person comes into the library, because there is an empty seat")).start();
         } else {
-            new Thread(() -> System.out.println("Человек стоит у двери библиотеки, так как библиотека заполнена")).start();
+            new Thread(() -> System.out.println("A person is standing at the door of the library, because the library is full")).start();
         }
 
 
@@ -82,7 +82,7 @@ public class TaskLibrary {
             int millisecondsToSleep = r.nextInt(4000) + 1000;
             try {
                 semaphore.acquire();
-                System.out.printf("Человек читает книгу " + millisecondsToSleep + " миллисекунд\n");
+                System.out.printf("A man reading a book " + millisecondsToSleep + " milliseconds\n");
 
                 Thread.sleep(millisecondsToSleep);
             } catch (InterruptedException e) {
@@ -91,7 +91,7 @@ public class TaskLibrary {
         }).start();
 
 
-        new Thread(() -> System.out.println("Человек вышел из библиотеки")).start();
+        new Thread(() -> System.out.println("The person left the library")).start();
 
     }
 }
